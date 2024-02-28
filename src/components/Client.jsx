2b53bd87311292042369df01d5198aa73e83b7da
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2024-02-28 22:41:32 +0530

    Update Client.jsx

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { PiFinnTheHumanFill } from "react-icons/pi";
 
 const Client = ({ userName }) => {
   return (
     <div className="client">
-      <img src="/vite.svg" width={30} height={30} alt="" />
+      <PiFinnTheHumanFill  />
+{/*       <img src="/vite.svg" width={30} height={30} alt="" /> */}
       <span className="userName">{userName}</span>
     </div>
   );
